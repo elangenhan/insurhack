264b64ab26d84776853054d75236bdd108c9503b
tohu <Snail Mail>
2016-11-19 15:07:33 +0100

    account+policies request and mockup fallback in case of error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var http = require('https');
 
 var zapi = require('./server/zapi.js');
 var mapi = require('./server/mapi.js');
+var chat = require('./server/chat.js');
 
 var httpsoptions = {
    key  : null, //fs.readFileSync('server.key'),
@@ -14,12 +15,32 @@ var httpsoptions = {
 
 app.use(express.static('www'));
 
+app.post('/api/chat', function(req, res) {
+    var input;
+    var response = chat.message(function(response) {
+        res.json(response);
+    }, input);
+});
+
+app.get('/api/policies', function(req, res) {
+    zapi.get_policies(function(data) {
+        res.json(data);
+    }, 'pc:1');
+});
+
+app.get('/api/account', function(req, res) {
+    zapi.get_account(function(data) {
+        res.json(data);
+    }, 'pc:1');
+});
+
 if(httpsoptions.key && httpsoptions.cert) {
     var https = require('https');
     https.createServer(options, app).listen(3000, function () {
         console.log('Started!');
     });
 }
+
 else {
     var http = require('http');
     http.createServer(app).listen(3000, function () {
--- server/zapi.js
+++ server/zapi.js
@@ -1,18 +1,91 @@
+var https = require('https');
+var jsonfile = require('jsonfile');
+
 var url_gi = "https://api.insurhack.com/apis/gi/1";
 var url_life = "https://api.insurhack.com/apis/life/1";
+var options_gi = {
+    hostname : "api.insurhack.com",
+    path : "",
+    grant_type : "password",
+    headers : {
+        Authorization : "Bearer ca125024-ea65-3588-b803-89b0a2f9c5fd"
+    },
+    method : "GET",
+    json:true,
+    port:443
+};
+var options_life = {
+    hostname : "api.insurhack.com",
+    path : "/apis/life/1"
+};
 
 var mofferings = {};
 
-exports.offerings = function(callback) {
-    https.get(url_gi, (res) => {
+function get_mocked(file) {
+    var data = jsonfile.readFileSync('./server/data/'+file);
+    console.log();
+    console.log("Using mocked data:");
+    console.log(data);
+    return data;
+}
+function get_object(d, mock_file) {
+    try {
+        return JSON.parse(d);
+    } catch(e) {
+        console.log(e);
+        return get_mocked(mock_file);
+    }
+}
+exports.get_policies = function(callback, id) {
+    // Account_Set('pc:1')?$expand=Policies($expand=BoundPeriods)
+    var mockfile = "policies.json.json";
+    var path = "/apis/gi/1/Account_Set('" + id + "')?$expand=Policies($expand=BoundPeriods)";
+
+    var opt = options_gi;
+    opt.path = path;
+    var req = https.request(options_gi, (res) => {
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
 
         res.on('data', (d) => {
             process.stdout.write(d);
+            var obj = get_object(d,mockfile);
+            callback(obj);
         });
-    }).on('error', (e) => {
+    })
+    req.end();
+    req.on('error', (e) => {
         console.error(e);
-        callback(mofferings);
+        callback(e);
+        var data = get_mocked(mockfile);
+        data.error = e;
+        callback(data);
+    });
+}
+
+exports.get_account = function(callback, id) {
+    // /Account_Set('pc:1')?$expand=AccountHolderContact
+    var mockfile = "account.json";
+    var path = "/apis/gi/1/Account_Set('" + id + "')?$expand=AccountHolderContact";
+
+    var opt = options_gi;
+    opt.path = path;
+    var req = https.request(options_gi, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+            var obj = get_object(d, mockfile);
+            callback(obj);
+        });
+    })
+    req.end();
+    req.on('error', (e) => {
+        console.error(e);
+        callback(e);
+        var data = get_mocked(mockfile);
+        data.error = e;
+        callback(data);
     });
 }
