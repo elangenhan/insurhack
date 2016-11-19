daf4b9b532060b4161ed16c9a55b2bbe00ee57d9
elangenhan <erik.langenhan@web.de>
2016-11-19 15:33:01 +0100

    add smileys

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -9,10 +9,14 @@ export class DetailPage {
 
     private data;
     private kpi1;
+    private kpi2;
+    private kpi3;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.data = this.navParams.get('data');
       this.kpi1 = 4;
+      this.kpi2 = 1;
+      this.kpi3 = 5;
   }
 
   log(data) {
