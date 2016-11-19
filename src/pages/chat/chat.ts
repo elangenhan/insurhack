cb4e32b72f64d074909e5d6761cbb8972ed007c8
elangenhan <erik.langenhan@web.de>
2016-11-19 22:52:27 +0100

    implement chat

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,29 +1,60 @@
 import { Component } from '@angular/core';
-
+import { ChatService } from '../../providers/chat-service';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-chat',
-  templateUrl: 'chat.html'
+  templateUrl: 'chat.html',
+  providers: [ChatService]
 })
 export class ChatPage {
 
-  private messages;
+  private messages = [];
   private messageInput;
+  private context = {};
 
-  constructor(public navCtrl: NavController) {
-      this.messages = [{
-          align: "left",
-          message: "Hello, how may I help you?"
-      }];
+  constructor(public navCtrl: NavController, public chatService: ChatService) {
+      this.init();
+  }
+
+  init() {
+    this.chatService.sendMessage({
+        workspace_id: '61970c56-80d8-48eb-838b-6fdd890f85b6',
+        input: {'text': "Hello"},
+        context: this.context
+      }).then(data => {
+        this.context = data.context;
+        let tmp = data.output.text;
+        for (let j in tmp) {
+          this.messages.push({
+            align: "left",
+            message: tmp[j]
+          })
+        }
+      })
   }
 
   sendMessage(input: string) {
     if(input != "" && input != undefined) {
+      this.chatService.sendMessage({
+        workspace_id: '61970c56-80d8-48eb-838b-6fdd890f85b6',
+        input: {'text': input},
+        context: this.context
+      }).then(data => {
+        console.log(data);
+        this.context = data.context;
+        let tmp = data.output.text;
+        for (let j in tmp) {
+          this.messages.push({
+            align: "left",
+            message: tmp[j]
+          })
+        }
+      })
       this.messages.push({
           align: "right",
           message: input
-      })
+        })
     }
 
       this.messageInput = "";
---
+++ src/providers/chat-service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the ChatService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class ChatService {
+
+    data;
+
+  constructor(public http: Http) {
+    console.log('Hello ChatService Provider');
+  }
+
+  sendMessage(message) {
+      return new Promise(resolve => {
+        // We're using Angular HTTP provider to request the data,
+        // then on the response, it'll map the JSON data to a parsed JS object.
+        // Next, we process the data and resolve the promise with the new data.
+        this.http.post('http://localhost:3000/api/chat', message)
+          .map(res => res.json())
+          .subscribe(data => {
+            // we've got back the raw data, now generate the core schedule data
+            // and save the data for later reference
+            this.data = data;
+            resolve(this.data);
+            return this.data;
+          });
+      });
+    }
+
+}
